5b3511d11ab8c2166a9d3fabdb6efbae7fa9732f
Vlad Shvedov <vshvedov@heliostech.hk>
2012-03-26 13:14:39 +0300

    Init

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+gem 'omniauth', '>= 1.0.0'
+gem 'omniauth-idnet', :git => 'git://github.com/helios-technologies/omniauth-idnet.git'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdnetRails3Example::Application.config.secret_token = '07b718a5220a312bfe6dd115c8ad39c0f144d0827e06f81f3cb0105eb46238b2d04f50c3a78b023fc30c03ba1e955769ff851f8d141f7c142fdd86d474601893'
